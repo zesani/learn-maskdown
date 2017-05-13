d4696e19d44bbfffe170be93b3efaf82c7a9eff4
zesani <7sin@outlook.co.th>
2017-05-13 09:29:29 +0700

    init

---
+++ src/pages/init.vue
@@ -0,0 +1,34 @@
+<template lang="html">
+  <div class="">
+    <div id="editor">
+      <textarea v-model="input"></textarea>
+      {{input}}
+      <div v-html="compiledMarkdown"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+/* global marked */
+export default {
+  data () {
+    return {
+      input: ''
+    }
+  },
+  computed: {
+    compiledMarkdown: function () {
+      return marked(this.input, { sanitize: true })
+    }
+  },
+  methods: {
+    // update: _.debounce(function (e) {
+    //   this.input = e.target.value
+    // }, 300)
+  }
+}
+
+</script>
+
+<style lang="css">
+</style>
